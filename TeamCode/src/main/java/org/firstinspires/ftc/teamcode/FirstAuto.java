b5531904f58466842c0a3044911bff6b65e59648
graffshayna <graffshayna@gmail.com>
2025-10-08 16:29:43 -0400

    Learned Hardware and Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAuto.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Auto")
+public class FirstAuto extends LinearOpMode {
+
+    Hardware robot = Hardware.getInstance();
+
+    //essentially our main method
+    public void runOpMode(){
+        robot.inti(hardwareMap);
+        //always add telemetry.update() to make sure telemetry runs repeatedly
+        telemetry.addData("Status", "Hello, Drivers!");
+        telemetry.update();
+
+        waitForStart();
+        //once auto starts
+        move(6, 0.67);
+        move(10, 0.3);
+        turning(800, 0.2);
+
+    }
+    public void move(double distance, double speed){
+        double wheelCircumference = 4 * Math.PI;
+        double motor = 560;
+        double ticks = (distance * (motor/wheelCircumference));
+
+        robot.rf.setTargetPosition((int)Math.round(ticks));
+        robot.lf.setTargetPosition((int)Math.round(ticks));
+        robot.rb.setTargetPosition((int)Math.round(ticks));
+        robot.lb.setTargetPosition((int)Math.round(ticks));
+
+        robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.setPower(speed, speed, speed, speed);
+
+        while (opModeIsActive() && robot.lb.isBusy()){
+
+        }
+        robot.setPower(0, 0, 0, 0);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+    }
+
+    public void turning(int ticks, double speed){
+        robot.rf.setTargetPosition(ticks);
+        robot.lf.setTargetPosition(-ticks);
+        robot.rb.setTargetPosition(ticks);
+        robot.lb.setTargetPosition(-ticks);
+
+        robot.rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.rb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.lb.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.setPower(speed, speed, -speed, -speed);
+
+        while (opModeIsActive() && robot.lb.isBusy()){
+
+        }
+        robot.setPower(0, 0, 0, 0);
+
+        robot.rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+public class Hardware {
+
+    //Time based: doesn't account for skid
+    //encoder: measures how many time the magnets pass, doesn't account for slip
+    //odometry: corrects position and sees exact location
+
+    public DcMotor rf;
+    public DcMotor lf;
+    public DcMotor rb;
+    public DcMotor lb;
+
+    public Servo demoServo;
+
+    public static double maxSpeed = 0.9;
+
+    private static Hardware myInstance = null;
+    public static Hardware getInstance() {
+        if (myInstance == null){
+            myInstance = new Hardware();
+        }
+        return myInstance;
+    }
+
+
+    public void inti(HardwareMap hwMap) {
+        rf = hwMap.get(DcMotor.class, "rf");
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rf.setPower(0);
+
+        lf = hwMap.get(DcMotor.class, "lf");
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lf.setPower(0);
+
+        rb = hwMap.get(DcMotor.class, "rb");
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rb.setPower(0);
+
+        lb = hwMap.get(DcMotor.class, "lb");
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lb.setPower(0);
+
+        //Initialize servo
+
+        demoServo = hwMap.get(Servo.class, "demoServo");
+    }
+
+    public void setPower(double fr, double br, double bl, double fl){
+
+        rf.setPower(Range.clip(fr, -maxSpeed, maxSpeed));
+        lf.setPower(Range.clip(fl, -maxSpeed, maxSpeed));
+        rb.setPower(Range.clip(br, -maxSpeed, maxSpeed));
+        lb.setPower(Range.clip(bl, -maxSpeed, maxSpeed));
+    }
+}
