c76d020c2d68b94c8f2717b2ed934819f3a4bcc9
tmayida28-rgb <tmayida28@gmail.com>
2025-10-24 15:00:52 -0400

    teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryTeleOp.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp (name = "Demo for 3839")
+public class OdometryTeleOp extends LinearOpMode {
+    public DcMotorEx rf;
+    public DcMotorEx rb;
+    public DcMotorEx lf;
+    public DcMotorEx lb;
+    public void init(HardwareMap hwMap) {
+
+        rf = hwMap.get(DcMotorEx.class, "rf");
+        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf.setPower(0);
+        //1
+
+        rb = hwMap.get(DcMotorEx.class, "rr");
+        rb.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setPower(0);
+        //3
+
+        lf = hwMap.get(DcMotorEx.class, "lf");
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        lf.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf.setPower(0);
+        //0
+
+        lb = hwMap.get(DcMotorEx.class, "lr");
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setPower(0);
+    }
+
+    public void runOpMode(){
+        double forward, sideways, turning, max;
+        double scaleFactor = 0.8;
+
+        init(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+
+            forward = -(Math.atan(5 * -gamepad1.left_stick_y) / Math.atan(5));
+            sideways = (Math.atan(5 * gamepad1.left_stick_x) / Math.atan(5));
+            turning = (Math.atan(5 * -gamepad1.right_stick_x) / Math.atan(5)) * 0.5;
+            max = Math.max(Math.abs(forward - sideways - turning), Math.max(Math.abs(forward + sideways - turning), Math.max(Math.abs(forward + sideways + turning), Math.abs(forward + turning - sideways))));
+            if (max > 1) {
+                scaleFactor = 1 / max;
+            } else {
+                scaleFactor = 1;
+            }
+            scaleFactor *= Math.max(Math.abs(1 - gamepad1.right_trigger), 0.2);
+            setPower((forward - sideways - turning) * scaleFactor, (forward + sideways - turning) * scaleFactor, (forward + sideways + turning) * scaleFactor, (forward + turning - sideways) * scaleFactor);
+        }
+    }
+    public void setPower(double fr, double br, double fl, double bl){
+        rf.setPower(Range.clip(fr, -1, 1));
+        rb.setPower(Range.clip(br, -1, 1));
+        lf.setPower(Range.clip(fl, -1, 1));
+        lb.setPower(Range.clip(bl, -1, 1));
+    }
+}
+
+
+
