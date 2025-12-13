786e350aa660a6a6d9793b4603f3bc55d00cceab
Chingwonding <muchuan.ccc@gmail.com>
2025-12-12 21:15:02 -0500

    fixing auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnotherAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnotherAuto.java
@@ -1,11 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-
-
- // make sure this aligns with class location
-
-
-
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
@@ -13,273 +7,190 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Autonomous(name = "Example Auto", group = "Examples")
+@Autonomous(name = "Fixed Example Auto", group = "Examples")
 public class AnotherAuto extends OpMode {
 
+    Hardware robot = Hardware.getInstance();
+
     private Follower follower;
-    private Timer pathTimer, actionTimer, opmodeTimer;
+    private Timer pathTimer, opmodeTimer;
 
     private int pathState;
 
+    // Define Poses
     private final Pose startPose = new Pose(120, -120, 90);
-
-
+    private final Pose shotPosition = new Pose(80, 65, 135);
 
     private final Pose beforePickUp1 = new Pose(80, -70, Math.toRadians(180));
     private final Pose afterPickUp1 = new Pose(20, 70, Math.toRadians(180));
 
-
-    //this is the shot position
-
-    private final Pose shotPosition = new Pose(80, 65, 135);
-
-
-
-
     private final Pose beforePickUp2 = new Pose(75, -25, Math.toRadians(180));
     private final Pose afterPickUp2 = new Pose(20, -25, Math.toRadians(135));
-
-
-
 
     private final Pose beforePickUp3 = new Pose(70, 25, Math.toRadians(180));
     private final Pose afterPickUp3 = new Pose(20, 25, Math.toRadians(180));
 
+    // Declare Paths
+    private Path initialPath;
+    private PathChain scoreCycle1, grabCycle2, scoreCycle2, grabCycle3, scoreCycle3;
 
-    private Path scoreLoad;
-    //private PathChain grabPickup1, scorePickup1, grabPickup2, scorePickup2, grabPickup3, scorePickup3;
-
-    Hardware robot = Hardware.getInstance();
-    private PathChain startToBeforeOne;
-    private PathChain beforeAfterOne;
-    private PathChain AfterBeforeOne;
-    private PathChain ShotSequenceOne;
-    private PathChain shotStartTwo;
-    private PathChain ShotSequenceTwo;
-    private PathChain shotStartThree;
-
-    public void buildPaths() {
-
-        scoreLoad = new Path(new BezierLine(startPose, beforePickUp1));
-        scoreLoad.setLinearHeadingInterpolation(startPose.getHeading(), beforePickUp1.getHeading());
-
-        PathChain beforeAfterOne = follower.pathBuilder()
-                .addPath(new BezierLine(beforePickUp1, afterPickUp1))
-                .setLinearHeadingInterpolation(beforePickUp1.getHeading(), afterPickUp1.getHeading())
-                .build();
-
-        PathChain AfterBeforeOne = follower.pathBuilder()
-                .addPath(new BezierLine(afterPickUp1, beforePickUp1))
-                .setLinearHeadingInterpolation(afterPickUp1.getHeading(), shotPosition.getHeading())
-                .build();
-
-        PathChain ShotSequenceOne = follower.pathBuilder()
-                .addPath(new BezierLine(shotPosition,beforePickUp2))
-                .setLinearHeadingInterpolation(shotPosition.getHeading(), beforePickUp2.getHeading())
-                .build();
-
-        PathChain beforeAfterTwo = follower.pathBuilder()
-                .addPath(new BezierLine(beforePickUp2, afterPickUp2))
-                .setLinearHeadingInterpolation(beforePickUp2.getHeading(), afterPickUp2.getHeading())
-                .build();
-
-        PathChain afterBeforeTwo = follower.pathBuilder()
-                .addPath(new BezierLine(afterPickUp2, beforePickUp2))
-                .setLinearHeadingInterpolation(afterPickUp2.getHeading(), beforePickUp2.getHeading())
-                .build();
-
-        PathChain ShotSequenceTwo = follower.pathBuilder()
-                .addPath(new BezierLine(beforePickUp2,shotPosition))
-                .setLinearHeadingInterpolation(beforePickUp2.getHeading(), shotPosition.getHeading())
-                .build();
-
-        PathChain beforeAfterThree = follower.pathBuilder()
-                .addPath(new BezierLine(beforePickUp3, afterPickUp3))
-                .setLinearHeadingInterpolation(beforePickUp3.getHeading(), afterPickUp3.getHeading())
-                .build();
-
-        PathChain afterBeforeThree = follower.pathBuilder()
-                .addPath(new BezierLine(afterPickUp3, beforePickUp3))
-                .setLinearHeadingInterpolation(afterPickUp3.getHeading(), beforePickUp3.getHeading())
-                .build();
-
-        PathChain shotSequenceThree = follower.pathBuilder()
-                .addPath(new BezierLine(beforePickUp3, shotPosition))
-                .setLinearHeadingInterpolation(beforePickUp3.getHeading(), shotPosition.getHeading())
-                .build();
-
-                readyToShot();
-
-
-
-
-
-        
-
-
-        /*
-hello
-
-         This is our scorePreload path. We are using a BezierLine, which is a straight line.
-        scorePreload = new Path(new BezierLine(startPose, scorePose));
-        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
-
-                 Here is an example for Constant Interpolation
-            scorePreload.setConstantInterpolation(startPose.getHeading());
-
-          This is our grabPickup1 PathChain. We are using a single path with a BezierLine, which is a straight line.
-
-        grabPickup1 = follower.pathBuilder()
-                .addPath(new BezierLine(scorePose, pickup1Pose))
-                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
-                .build();
-
-        /* This is our scorePickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-
-
-
-
-
-
-    }
-
-
-    public void readyToShot()
-    {
-        robot.shotMotorOne.setPower(0.7);
-
-        robot.shotMotorTwo.setPower(0.7);
-
-        robot.UpServo.setPosition(0.428);
-
-    }
-
-    public void autonomousPathUpdate() {
-        switch (pathState) {
-            case 0:
-                follower.followPath(scoreLoad);
-                setPathState(1);
-                break;
-            case 1:
-                follower.followPath(scoreLoad);
-
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
-                if (!follower.isBusy()) {
-                    follower.followPath(beforeAfterOne, true);
-                    setPathState(2);
-                }
-                break;
-                /*
-            case 2:
-                 This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position
-                if (!follower.isBusy()) {
-
-                    follower.followPath(beforeAfterOne, true);
-                    setPathState(3);
-                }
-                break;
-            case 3:
-                if (!follower.isBusy()) {
-                    follower.followPath(beforePickUp3, true);
-                    setPathState(4);
-                }
-                break;
-            case 4:
-                if (!follower.isBusy()) {
-                    setPathState(5);
-                }
-                break;
-            case 5:
-                if (!follower.isBusy()) {
-                    follower.followPath(grabPickup3, true);
-                    setPathState(6);
-                }
-                break;
-            case 6:
-                if (!follower.isBusy()) {
-                    follower.followPath(, true);
-                    setPathState(7);
-                }
-                break;
-            case 7:
-                if (!follower.isBusy()) {
-                    setPathState(-1);
-                }
-                break;
-
-                 */
-        }
-    }
-
-    /**
-     * These change the states of the paths and actions. It will also reset the timers of the individual switches
-     **/
-    public void setPathState(int pState) {
-        pathState = pState;
-        pathTimer.resetTimer();
-    }
-
-    /**
-     * This is the main loop of the OpMode, it will run repeatedly after clicking "Play".
-     **/
-    @Override
-    public void loop() {
-
-        // These loop the movements of the robot, these must be called continuously in order to work
-        follower.update();
-        autonomousPathUpdate();
-
-        // Feedback to Driver Hub for debugging
-        telemetry.addData("path state", pathState);
-        telemetry.addData("x", follower.getPose().getX());
-        telemetry.addData("y", follower.getPose().getY());
-        telemetry.addData("heading", follower.getPose().getHeading());
-        telemetry.update();
-    }
-
-    /**
-     * This method is called once at the init of the OpMode.
-     **/
     @Override
     public void init() {
+        robot.init(hardwareMap);
         pathTimer = new Timer();
         opmodeTimer = new Timer();
-        opmodeTimer.resetTimer();
-
 
         follower = Constants.createFollower(hardwareMap);
         buildPaths();
         follower.setStartingPose(startPose);
-
     }
 
-    /**
-     * This method is called continuously after Init while waiting for "play".
-     **/
     @Override
     public void init_loop() {
+        // Can be used for vision processing before start
     }
 
-    /**
-     * This method is called once at the start of the OpMode.
-     * It runs all the setup actions, including building paths and starting the path system
-     **/
     @Override
     public void start() {
         opmodeTimer.resetTimer();
         setPathState(0);
     }
 
-    /**
-     * We do not use this because everything should automatically disable
-     **/
+    @Override
+    public void loop() {
+        follower.update();
+        autonomousPathUpdate();
+
+        telemetry.addData("Path State", pathState);
+        telemetry.addData("X", follower.getPose().getX());
+        telemetry.addData("Y", follower.getPose().getY());
+        telemetry.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
+        telemetry.update();
+    }
+
     @Override
     public void stop() {
     }
 
-}
+    public void buildPaths() {
+        // Path names have been updated for clarity and logical flow.
 
+        // 1. Start to a point near the first pickup
+        initialPath = new Path(new BezierLine(startPose, beforePickUp1));
+        initialPath.setLinearHeadingInterpolation(startPose.getHeading(), beforePickUp1.getHeading());
+
+        // 2. From near pickup 1, go to score position
+        scoreCycle1 = follower.pathBuilder()
+                .addPath(new BezierLine(beforePickUp1, afterPickUp1))
+                .addPath(new BezierLine(afterPickUp1, shotPosition))
+                .setLinearHeadingInterpolation(beforePickUp1.getHeading(), shotPosition.getHeading())
+                .build();
+
+        // 3. From score position, go to grab second element
+        grabCycle2 = follower.pathBuilder()
+                .addPath(new BezierLine(shotPosition, beforePickUp2))
+                .setLinearHeadingInterpolation(shotPosition.getHeading(), beforePickUp2.getHeading())
+                .build();
+
+        // 4. From near pickup 2, go to score position
+        scoreCycle2 = follower.pathBuilder()
+                .addPath(new BezierLine(beforePickUp2, afterPickUp2))
+                .addPath(new BezierLine(afterPickUp2, shotPosition))
+                .setLinearHeadingInterpolation(beforePickUp2.getHeading(), shotPosition.getHeading())
+                .build();
+
+        // 5. From score position, go to grab third element
+        grabCycle3 = follower.pathBuilder()
+                .addPath(new BezierLine(shotPosition, beforePickUp3))
+                .setLinearHeadingInterpolation(shotPosition.getHeading(), beforePickUp3.getHeading())
+                .build();
+
+        // 6. From near pickup 3, go to score position
+        scoreCycle3 = follower.pathBuilder()
+                .addPath(new BezierLine(beforePickUp3, afterPickUp3))
+                .addPath(new BezierLine(afterPickUp3, shotPosition))
+                .setLinearHeadingInterpolation(beforePickUp3.getHeading(), shotPosition.getHeading())
+                .build();
+    }
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                // Start moving along the initial path
+                follower.followPath(initialPath);
+                setPathState(1);
+                break;
+            case 1:
+                // Wait for initial path to finish, then start first scoring path
+                if (!follower.isBusy()) {
+                    readyToShoot();
+                    follower.followPath(scoreCycle1);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                // Wait for score 1 to finish, do action (e.g. shoot), then grab 2nd element
+                if (!follower.isBusy()) {
+                    // shoot();
+                    follower.followPath(grabCycle2);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+                // Wait for grab 2 to finish, do action (e.g. pickup), then start score 2
+                if (!follower.isBusy()) {
+                    // pickup();
+                    readyToShoot();
+                    follower.followPath(scoreCycle2);
+                    setPathState(4);
+                }
+                break;
+            case 4:
+                 // Wait for score 2 to finish, do action (e.g. shoot), then grab 3rd element
+                if (!follower.isBusy()) {
+                    // shoot();
+                    follower.followPath(grabCycle3);
+                    setPathState(5);
+                }
+                break;
+            case 5:
+                // Wait for grab 3 to finish, do action (e.g. pickup), then start score 3
+                if (!follower.isBusy()) {
+                    // pickup();
+                    readyToShoot();
+                    follower.followPath(scoreCycle3);
+                    setPathState(6);
+                }
+                break;
+            case 6:
+                // Wait for final score path to finish
+                if (!follower.isBusy()) {
+                    // shoot();
+                    setPathState(7); // Pathing is complete
+                }
+                break;
+            case 7:
+                // Autonomous is finished.
+                break;
+        }
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    public void readyToShoot() {
+        robot.shotMotorOne.setPower(0.7);
+        robot.shotMotorTwo.setPower(0.7);
+        robot.UpServo.setPosition(0.428);
+    }
+
+    // It would be a good idea to add other action methods like these:
+    // public void shoot() { ... }
+    // public void pickup() { ... }
+}
